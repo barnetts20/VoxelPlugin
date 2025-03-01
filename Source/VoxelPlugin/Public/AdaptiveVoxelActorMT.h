dd36586c22fe24821ed75415f63416e83146eaa0
snuzuns <barnetts20@gmail.com>
2025-03-01 03:56:30 -0500

    Updates to actor wrapper

--- Source/VoxelPlugin/Public/AdaptiveVoxelActorMT.h
+++ Source/VoxelPlugin/Public/AdaptiveVoxelActorMT.h
@@ -4,23 +4,55 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "FAdaptiveOctreeMT.h"
 #include "AdaptiveVoxelActorMT.generated.h"
 
 UCLASS()
-class VOXELPLUGIN_API AAdaptiveVoxelActorMT : public AActor
+class VOXELPLUGIN_API AAdaptiveVoxelActorMT : public ARealtimeMeshActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	AAdaptiveVoxelActorMT();
+private:
+    TSharedPtr<FAdaptiveOctreeMT> AdaptiveOctree;
+    TSharedPtr<FSparseOctree> SparseOctree;
+
+    FCameraInfo CameraData;
+    FCameraInfo LastCameraData;
+  
+    FRWLock OctreeLock;
+
+    int ChunkDepth = 4;
+    int MinDepth = 7;
+    int MaxDepth = 16;
+    int LodFactor = 12;
+    int CollisionDepth = 14;
+
+    bool TickInEditor = false;
+    bool Initialized = false;
+
+public:
+    AAdaptiveVoxelActorMT();
+    virtual void OnConstruction(const FTransform& Transform) override;
+    virtual void BeginPlay() override;
+    virtual void BeginDestroy() override;
+    virtual bool ShouldTickIfViewportsOnly() const override;
+    virtual void Tick(float DeltaTime) override;
+
+    TSharedPtr<FAdaptiveOctreeMT> GetOctree();
+
+    // Material properties
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Materials")
+    UMaterialInterface* Material;
+
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    void CleanSceneRoot();
+    void InitializeChunks();
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    //Async
+    bool DataUpdateIsRunning = false;
+    void ScheduleDataUpdate(float IntervalInSeconds);
+    bool MeshUpdateIsRunning = false;
+    void ScheduleMeshUpdate(float IntervalInSeconds);
 
 };
