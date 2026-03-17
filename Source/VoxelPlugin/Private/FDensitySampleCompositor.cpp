afbac5f2e56d97e68e81a988bbbc640bdd1236ee
snuzuns <barnetts20@gmail.com>
2026-03-17 18:05:57 -0400

    Add cpp back in for example methods

---
+++ Source/VoxelPlugin/Private/FDensitySampleCompositor.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FDensitySampleCompositor.h"
+
+FDensitySampleCompositor::FDensitySampleCompositor()
+{
+}
+
+FDensitySampleCompositor::~FDensitySampleCompositor()
+{
+}
